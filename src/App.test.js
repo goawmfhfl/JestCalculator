085d5ce5c2f90d98999fae5a98bf3be5a6c5133a
jae <goawmfhfl1@naver.com>
2022-04-20 08:53:47 +0900

    Add: 0420Study

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,20 @@ import App from "./App";
 
 test("the counter start at 0", () => {
   render(<App />);
+  // data-testid="counter"
   const counterElement = screen.queryByTestId("counter");
+  // <h3 data-testid="counter">{counter}</h3> 안에있는 요소 접근하기
   expect(counterElement).toHaveTextContent(0);
+});
+
+test("minus button has correct test", () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toHaveTextContent("-");
+});
+
+test("plut button has correct test", () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
 });
