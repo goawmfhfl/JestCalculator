3ee9f795b5f5f8c41e838e2a42d3782384fc4a61
jae <goawmfhfl1@naver.com>
2022-04-29 16:57:57 +0900

    Test: checkBox and button Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SummaryPage from "./pages/SummaryPage/SummaryPage";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <SummaryPage />
+    </div>
+  );
 }
 
 export default App;
